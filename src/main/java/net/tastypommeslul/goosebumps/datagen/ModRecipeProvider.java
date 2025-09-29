fbd11e9760232dae92ae20050ec37cd1d963ffc7
TastyPommesLul <ernstd.privat@gmail.com>
2025-09-29 20:33:07 +0200

    Added a Spear Head and Recipe for it.

--- src/main/java/net/tastypommeslul/goosebumps/datagen/ModRecipeProvider.java
+++ src/main/java/net/tastypommeslul/goosebumps/datagen/ModRecipeProvider.java
@@ -5,7 +5,6 @@ import net.fabricmc.fabric.api.datagen.v1.provider.FabricRecipeProvider;
 import net.minecraft.data.recipe.RecipeExporter;
 import net.minecraft.data.recipe.RecipeGenerator;
 import net.minecraft.data.recipe.ShapedRecipeJsonBuilder;
-import net.minecraft.data.recipe.ShapelessRecipeJsonBuilder;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.book.RecipeCategory;
 import net.minecraft.registry.Registries;
