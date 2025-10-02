29fc4fcb7e40ac80a26d55623815d81519672f3a
TastyPommesLul <ernstd.privat@gmail.com>
2025-10-02 16:19:12 +0200

    Added full ModMenu Implementation and a bare-bones Lattice Config screen.

--- src/main/java/net/tastypommeslul/goosebumps/compat/ModMenuImpl.java
+++ src/main/java/net/tastypommeslul/goosebumps/compat/ModMenuImpl.java
@@ -1,11 +1,9 @@
 package net.tastypommeslul.goosebumps.compat;
 
-import com.terraformersmc.modmenu.api.ConfigScreenFactory;
-import com.terraformersmc.modmenu.api.ModMenuApi;
+import com.terraformersmc.modmenu.api.*;
 import net.tastypommeslul.goosebumps.client.GoosebumpsClient;
 
 public class ModMenuImpl implements ModMenuApi {
-
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
         return GoosebumpsClient::createConfigScreen;
